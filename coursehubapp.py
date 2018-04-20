8a190dfb8e5baab742cd56c40ef7901e0a3bd487
Rifat Noor <rifatnoor45@yahoo.com>
2018-04-20 10:33:14 -0400

    Updated Databases

--- coursehubapp.py
+++ coursehubapp.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash, session
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm 
-from wtforms import StringField, PasswordField, BooleanField
+from wtforms import StringField, PasswordField, BooleanField, TextAreaField
 from wtforms.validators import InputRequired, Email, Length
 from flask_sqlalchemy  import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -10,8 +10,8 @@ from time import sleep
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'Coursehubssecretkey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/davidliao/Desktop/Capstone/coursehubmembers.db'
+app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/rifatnoor/CourseHub/courseHub.db'
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 login_manager = LoginManager()
@@ -19,78 +19,123 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(15), unique=True)
-    email = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(80))
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(15), unique=True, nullable=False)
+	email = db.Column(db.String(50), unique=True, nullable=False)
+	password = db.Column(db.String(80), nullable=False)
+	posts = db.relationship('Post', backref='user', lazy=True)
+
+class Post(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(100))
+	description = db.Column(db.String())
+	post_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+	return User.query.get(int(user_id))
 
 class LoginForm(FlaskForm):
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
-    remember = BooleanField('remember me')
+	username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+	password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+	remember = BooleanField('remember me')
 
 class RegisterForm(FlaskForm):
-    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+	email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+	username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+	password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+
+class PostForm(FlaskForm):
+	title = StringField('Title', validators=[InputRequired()])
+	description = TextAreaField('Text', validators=[InputRequired()])
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
+	form = LoginForm()
 
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user:
-            if check_password_hash(user.password, form.password.data):
-                login_user(user, remember=form.remember.data)
-                return redirect(url_for('userspage'))
+	if form.validate_on_submit():
+		user = User.query.filter_by(username=form.username.data).first()
+		if user:
+			if check_password_hash(user.password, form.password.data):
+				login_user(user, remember=form.remember.data)
+				session['username'] = form.username.data
+				return redirect(url_for('userspage'))
 
-        flash("Invalid username or password!")
+		flash("Invalid username or password!")
+		
+		return render_template('login.html', form=form)
 
-        return render_template('login.html', form=form)
-
-    return render_template('login.html', form=form)
+	return render_template('login.html', form=form)
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    form = RegisterForm()
+	form = RegisterForm()
 
-    if form.validate_on_submit():
-        hashed_password = generate_password_hash(form.password.data, method='sha256')
-        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
+	if form.validate_on_submit():
+		hashed_password = generate_password_hash(form.password.data, method='sha256')
+		new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+		db.session.add(new_user)
+		db.session.commit()
 
-        flash("Signup sucessful!")
+		flash("Signup sucessful!")
 
-        return render_template('signup.html', form=form)
+		return render_template('signup.html', form=form)
 
-    return render_template('signup.html', form=form)
+	return render_template('signup.html', form=form)
 
 @app.route('/user')
 @login_required
 def userspage():
-    return render_template('userspage.html', name=current_user.username)
+	return render_template('userspage.html', name=current_user.username)
 
 @app.route('/coursesearch')
 @login_required
 def coursesearch():
-    return render_template('coursesearch.html')
+	return render_template('coursesearch.html')
+
+@app.route('/post', methods=['GET', 'POST'])
+@login_required
+def post():
+
+	form = PostForm()
+
+	if form.validate_on_submit():
+		user = User.query.filter_by(username=session['username']).first()
+		new_post = Post(title=form.title.data, description=form.description.data, post_id=user.id)
+		db.session.add(new_post)
+		db.session.commit()
+
+		flash("Sucess!")
+
+		return render_template('post.html', form=form)
+		
+	return render_template('post.html', form=form)
 
 @app.route('/logout')
 @login_required
 def logout():
-    logout_user()
-    return redirect(url_for('index'))
+	logout_user()
+	return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
